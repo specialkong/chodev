859f3b12e69f2a9ebbb6ebaecdc40dcbc7b9bca5
pureu <specialkong@daum.net>
2017-06-16 23:20:32 +0900

    week2 example commit

--- chodev/src/main/java/study/chodev/pureu/problem/week2/EvenFibonacci.java
+++ chodev/src/main/java/study/chodev/pureu/problem/week2/EvenFibonacci.java
@@ -2,4 +2,37 @@ package study.chodev.pureu.problem.week2;
 
 public class EvenFibonacci {
 
+	public void process(Object[] arg) {
+		
+		int max = (Integer)arg[0];
+		int odd = 1;
+		int even = 2;
+		int fibonacci = 0;
+		int evenSum = even;
+		int idx = 1;
+		
+		System.out.print(odd + " " + even + " ");
+		while(fibonacci < max) {
+			fibonacci = odd + even;
+			if(fibonacci > max) {
+				break;
+			}
+			System.out.print(fibonacci + " ");
+			if(idx++ % 2 == 1) {
+				odd = fibonacci;
+			} else {
+				even = fibonacci;
+				evenSum += even;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("evenSum : " + evenSum);
+	}
+	
+	public static void main(String[] args) {
+		EvenFibonacci evenFibonacci = new EvenFibonacci();
+		evenFibonacci.process(new Object[]{100});
+		evenFibonacci.process(new Object[]{4000000});
+	}
 }
--- chodev/src/main/java/study/chodev/pureu/problem/week2/Multiples35.java
+++ chodev/src/main/java/study/chodev/pureu/problem/week2/Multiples35.java
@@ -2,4 +2,42 @@ package study.chodev.pureu.problem.week2;
 
 public class Multiples35 {
 
+	public void process(Object[] arg) {
+		
+		int max = (Integer)arg[0];
+		int arg1 = (Integer)arg[1];
+		int arg2 = (Integer)arg[2];
+		int sum = 0;
+		
+		for(int i=1;i<max;i++) {
+			if(isMultiple(i, arg1)) {
+				System.out.print(i + " ");
+				sum += i;
+			} else if(isMultiple(i, arg2)) {
+				System.out.print(i + " ");
+				sum += i;
+			}
+			if(i % 100 == 0) {
+				System.out.println();
+			}
+		}
+		
+		System.out.println();
+		System.out.println("sum : " + sum);
+	}
+	
+	public boolean isMultiple(int num, int devider) {
+		if(num % devider == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main(String[] args) {
+		Multiples35 multiple = new Multiples35();
+		multiple.process(new Object[]{10, 3, 5});
+		multiple.process(new Object[]{1000, 3, 5});
+		
+	}
 }
