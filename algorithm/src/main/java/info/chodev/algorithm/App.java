f31107304f873b07bc94f715827fe96f17d7febc
pureu <pureu>
2017-10-21 10:23:17 +0900

    algorithm project init

---
+++ algorithm/src/main/java/info/chodev/algorithm/App.java
@@ -0,0 +1,13 @@
+package info.chodev.algorithm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
