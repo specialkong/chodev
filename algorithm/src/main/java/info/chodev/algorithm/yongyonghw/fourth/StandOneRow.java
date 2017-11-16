e7db62e6d20c2a8ca680ebbb0a6b5bd77063b55b
YongHwan <YongHwan@YongHwan-PC>
2017-11-17 08:19:52 +0900

    -

--- algorithm/src/main/java/info/chodev/algorithm/yongyonghw/fourth/StandOneRow.java
+++ algorithm/src/main/java/info/chodev/algorithm/yongyonghw/fourth/StandOneRow.java
@@ -1,18 +1,21 @@
 package info.chodev.algorithm.yongyonghw.fourth;
 
 import java.util.Comparator;
+import java.util.Scanner;
 
 public class StandOneRow {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Comparator<String> comparator = new Comparator<String>() {
-
-			public int compare(String arg0, String arg1) {
-				// TODO Auto-generated method stub
-				return 0;
-			}
-		};
+		int num;
+		String rowInfo;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("메세지를 입력하세요.");
+		num = scan.nextInt();
+		//엔터처리
+		scan.nextLine();
+		rowInfo = scan.nextLine();
+		System.out.println(rowInfo);
+		scan.close();
 	}
 
 }
