dabd8b7833e93fccd93a27b05cce879c534a3aca
YongHwan <YongHwan@YongHwan-PC>
2017-11-16 23:53:59 +0900

    .

---
+++ algorithm/src/main/java/info/chodev/algorithm/yongyonghw/fourth/StandOneRow.java
@@ -0,0 +1,18 @@
+package info.chodev.algorithm.yongyonghw.fourth;
+
+import java.util.Comparator;
+
+public class StandOneRow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Comparator<String> comparator = new Comparator<String>() {
+
+			public int compare(String arg0, String arg1) {
+				// TODO Auto-generated method stub
+				return 0;
+			}
+		};
+	}
+
+}
