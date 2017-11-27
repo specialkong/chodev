7cf47a361fce18dfde904b35d97412856b62679b
seohae <kimseohae96@naver.com>
2017-11-27 19:45:02 +0900

    seohae

--- algorithm/src/main/java/info/chodev/algorithm/seohae/sixth/NumDesc.java
+++ algorithm/src/main/java/info/chodev/algorithm/seohae/sixth/NumDesc.java
@@ -1,5 +1,49 @@
 package info.chodev.algorithm.seohae.sixth;
 
-public class NumDesc {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+import info.chodev.algorithm.pureu.first.HarshadNumber;
+
+/**
+ * 정수 내림차순
+ * @author seohae
+ * @sinco 2017.11.27
+ */
+public class NumDesc {
+	
+	private HarshadNumber harshadNumber = new HarshadNumber();
+	
+	/**
+	 * 내림차순으로 정렬
+	 * @param n
+	 */
+	public void reverseInt(int num){
+		
+		List<Integer> position = harshadNumber.getPosition(num);
+		
+		Collections.sort(position);
+		Collections.reverse(position);
+    
+		toInt(position);
+	}
+	
+	public void toInt(List<Integer> position){
+		String result ="";
+		
+		for(int i=0; i<position.size(); i++){
+			result += position.get(i);
+		}
+		
+		Integer.parseInt(result);
+		
+		System.out.println(result);
+	}
+  
+	// 아래는 테스트로 출력해 보기 위한 코드입니다.
+	public static void  main(String[] args){
+		NumDesc ri = new NumDesc();
+		ri.reverseInt(118372);
+	}
 }
