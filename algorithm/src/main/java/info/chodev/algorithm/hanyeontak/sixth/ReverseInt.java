6830f6ba35bfd223b94684a34c407844a66daa6e
한연탁 <@DESKTOP-R0PS6AB>
2017-12-01 20:17:09 +0900

    다 못 품

---
+++ algorithm/src/main/java/info/chodev/algorithm/hanyeontak/sixth/ReverseInt.java
@@ -0,0 +1,26 @@
+package info.chodev.algorithm.hanyeontak.sixth;
+import java.util.Arrays;
+
+public class ReverseInt {
+	public int reverseInt(int[] n){
+		int temp=0;
+    for(int i=0; i<n.length;i++){
+      for(int j=i+1;j<n.length;j++){
+        if(n[i] < n[j]){
+          temp=n[i];
+          n[i]=n[j];
+          n[j]=temp;
+        }}}
+   	return n[];
+	}
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ReverseInt ri = new ReverseInt();
+		System.out.println(ri.reverseInt({1,1,8,3,7,2}));
+	}
+}
+
+	
+  
+	
